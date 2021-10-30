bc4915d86ecf42c6afc0e8491caf4dd05a1803f1
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-10-30 07:41:46 -0700

    Create help.py

---
+++ nksama/plugins/help.py
@@ -0,0 +1,9 @@
+from pyrogram import filters
+from pyrogram.types.bots_and_keyboards import callback_game
+from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
+from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup 
+from nksama import bot ,help_message
+from typing import List , Any
+
+HELPP_TEXT = """Hello
+............"""
