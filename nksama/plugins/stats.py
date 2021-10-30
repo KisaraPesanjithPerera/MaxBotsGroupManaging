7256eac63ba8c766ef09131809b4ca1535ca060b
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-10-30 07:45:35 -0700

    Add files via upload

---
+++ nksama/plugins/stats.py
@@ -0,0 +1,29 @@
+from pyrogram import filters , Client
+from EMIlia import bot
+from pymongo import MongoClient 
+from EMIlia.db import MONGO_URL as db_url
+
+users_db = MongoClient(db_url)['users']
+col = users_db['USER']
+
+grps = users_db['GROUPS']
+
+
+@bot.on_message(filters.command("stats"))
+def stats(_,message):
+  users = col.find({})
+  mfs = []
+  for x in users:
+    mfs.append(x['user_id'])
+    
+  total = len(mfs)
+  
+  grp = grps.find({})
+  grps_ = []
+  for x in grp:
+    grps_.append(x['chat_id'])
+    
+  total_ = len(grps_)
+  
+  bot.send_message(message.chat.id , f"Total Users: {total}\nTotal Groups: {total_}")
+  
