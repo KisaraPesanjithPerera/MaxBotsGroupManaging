b08879f12e6a946dcf1f538e814375e6dd4d358f
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-10-30 07:15:27 -0700

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -1,5 +1,4 @@
 from pyrogram import filters , Client
-from redis import Redis
 import os 
 
 bot = Client(
@@ -10,25 +9,4 @@ bot = Client(
     plugins=dict(root=f"{__name__}/plugins")
 )
 
-r = os.environ.get("REDIS_URL").split(":")
-REDIS_PASSWORD = r[2]
-REDIS_PORT = r[1]
-
-REDIS_DB = Redis(
-    host=r[0],
-    password=REDIS_PASSWORD,
-    port=REDIS_PORT,
-    decode_responses=True,
-)
-
-REDIS_DB.ping()
-PYRO_SESSION = os.environ['PYRO_SESSION']
-
-musicbot = Client(
-    PYRO_SESSION,
-    api_id=os.environ.get('API_ID'),
-    api_hash=os.environ['API_HASH'],
-)
-
 help_message = []
-
