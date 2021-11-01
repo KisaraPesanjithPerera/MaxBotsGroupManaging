c4475513ebed22bbb25568a86062dd01f5616a6d
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-10-31 22:35:11 -0700

    Update notes.py

--- nksama/plugins/notes.py
+++ nksama/plugins/notes.py
@@ -75,6 +75,6 @@ def delnote(_,message):
 
 
 help_message.append({
-    "Module_Name": "notes",
+    "Module_Name": "📝 Notes 📝",
     "Notes_Help": "/addnote note name text - to add a note\n/delnote NoteName - to delete a note\ngetnote NoteName - get a note\n/notes - to get a list of notes in your chats"
 })
