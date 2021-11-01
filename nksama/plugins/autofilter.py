55de05ebf9da8e179d092fd540ceaad489891710
KisaraPesanjithPerera <88541663+KisaraPesanjithPerera@users.noreply.github.com>
2021-11-01 00:04:38 -0700

    Create autofilter.py

---
+++ nksama/plugins/autofilter.py
@@ -0,0 +1,8 @@
+import os
+from nksama import bot , help_message
+from pyrogram import filters
+
+from pyrogram import filters
+from nksama import help_message
+
+help_message.append({'Module_Name': '📂 Autofilter 📂'})
